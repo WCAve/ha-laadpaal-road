c6fc7330731c9062f278192b972ff18e1d31eb54
WCAve <woutervanesch@gmail.com>
2026-05-05 22:59:21 +0200

    tst

--- custom_components/road_ev/const.py
+++ custom_components/road_ev/const.py
@@ -1,7 +1,7 @@
 """Constanten voor de Road.io (E-flux) integratie."""
 from datetime import timedelta
 
-DOMAIN = "eflux_ev"
+DOMAIN = "road_ev"
 CONF_LOCATION_ID = "location_id"
 CONF_NAME = "name"
 SCAN_INTERVAL = timedelta(seconds=60)
